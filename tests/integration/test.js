5184df798dbc53f6e38feb75754bf31f5cc260d5
Matthijs Dabroek <dabroek@mollie.com>
2017-09-01 16:14:32 +0200

    test(integration test): Fixed the integration test.

--- tests/integration/test.js
+++ tests/integration/test.js
@@ -116,9 +116,9 @@ describe('payments', () => {
   it('should integrate', (done) => {
     mollieClient.payments.all()
       .then((payments) => {
-        let paymentExists = Promise.resolve();
+        let paymentExists;
 
-        if (!payments.length) {
+        if (!payments.length || payments[0].isExpired()) {
           paymentExists = mollieClient.payments.create({
             amount: 10.00,
             description: 'Integration test payment',
@@ -126,29 +126,42 @@ describe('payments', () => {
           })
             .then(payment => expect(payment).toBeDefined())
             .catch(err => expect(err).toBeNull());
+        } else {
+          paymentExists = Promise.resolve(payments[0]);
         }
 
-        paymentExists.then(() => {
-          mollieClient.payments_refunds.all({ paymentId: payments[0].id })
+        paymentExists.then((payment) => {
+          /**
+           * If you want to test the rest of this flow you have to
+           * manually set this payment to paid.
+           */
+          if (!payment.isPaid()) {
+            done();
+          }
+
+          mollieClient.payments_refunds.all({ paymentId: payment.id })
             .then((paymentRefunds) => {
-              let refundExists = Promise.resolve();
+              let refundExists;
 
               if (!paymentRefunds.length) {
                 refundExists = mollieClient.payments_refunds.create({
                   paymentId: payments[0].id,
                   amount: 5.00,
-                }).then((paymentRefund) => {
-                  expect(paymentRefund).toBeDefined();
+                }).then((result) => {
+                  expect(result).toBeDefined();
+                  return result;
                 }).catch((err) => {
                   expect(err).toBeNull();
                 });
+              } else {
+                refundExists = Promise.resolve(paymentRefunds[0]);
               }
 
-              refundExists.then(() => {
-                mollieClient.payments_refunds.get(paymentRefunds[0].id, {
+              refundExists.then((paymentRefund) => {
+                mollieClient.payments_refunds.get(paymentRefund.id, {
                   paymentId: payments[0].id,
-                }).then((paymentRefund) => {
-                  expect(paymentRefund).toBeDefined();
+                }).then((result) => {
+                  expect(result).toBeDefined();
                   done();
                 }).catch((err) => {
                   expect(err).toBeNull();
