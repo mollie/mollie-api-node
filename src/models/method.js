b266bf4c84303fbb177d1926ad35e1a9184bf109
Matthijs Dabroek <dabroek@mollie.com>
2018-02-13 12:19:59 +0100

    Remove the payment method IDs from the method model since they are not properly exposed and require too much maintenance.

--- src/models/method.js
+++ src/models/method.js
@@ -4,21 +4,6 @@ import MollieModel from 'mollie-model';
  * The `Method` model
  */
 export default class Method extends MollieModel {
-  static IDEAL = 'ideal';
-  static CREDITCARD = 'creditcard';
-  static MISTERCASH = 'mistercash';
-  static SOFORT = 'sofort';
-  static BANKTRANSFER = 'banktransfer';
-  static DIRECTDEBIT = 'directdebit';
-  static BITCOIN = 'bitcoin';
-  static PAYPAL = 'paypal';
-  static BELFIUS = 'belfius';
-  static PAYSAFECARD = 'paysafecard';
-  static PODIUMCADEAUKAART = 'podiumcadeaukaart';
-  static KBC = 'kbc';
-  static GIFTCARD = 'giftcard';
-  static INGHOMEPAY = 'inghomepay';
-
   constructor(props) {
     super(props);
 
