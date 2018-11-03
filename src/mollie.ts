27f3c51bb2dafc1da704445c34da9af572363680
Michael Dekker <git@michaeldekker.nl>
2018-11-03 08:08:01 +0100

    Expose models

--- src/mollie.ts
+++ src/mollie.ts
@@ -1,6 +1,16 @@
 import createHttpClient from './create-http-client';
 import createMollieApi from './create-mollie-api';
 
+// Expose models
+import * as chargeback from './models/chargeback';
+import * as customer from './models/customer';
+import * as mandate from './models/mandate';
+import * as method from './models/method';
+import * as payment from './models/payment';
+import * as refund from './models/refund';
+import * as subscription from './models/subscription';
+export { chargeback, customer, mandate, method, payment, refund, subscription };
+
 /**
  * Create Mollie client.
  * @since 2.0.0
@@ -14,5 +24,3 @@ export default function mollie(options: any = {}) {
 
   return createMollieApi({ httpClient });
 }
-
-export * from './models';
