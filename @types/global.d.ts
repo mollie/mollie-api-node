0d43531152cbbc749247d6d5768955cd3f717707
Michael Dekker <git@michaeldekker.nl>
2019-01-06 06:04:04 +0100

    Cleanup

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -20,7 +20,7 @@ declare namespace Mollie {
     | 'hu_HU'
     | 'pl_PL'
     | 'lv_LV'
-    | 'lt_LT';
+    | 'lt_LT'
 
   type Method =
     | 'bancontact'
@@ -37,7 +37,7 @@ declare namespace Mollie {
     | 'kbc'
     | 'paypal'
     | 'paysafecard'
-    | 'sofort';
+    | 'sofort'
 
   type ApiMode = 'test' | 'live';
 
