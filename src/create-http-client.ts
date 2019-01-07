ebab99d22157600a4b500a53669c234870587758
Michael Dekker <git@michaeldekker.nl>
2019-01-07 18:48:47 +0100

    Cleanup create-http-client

--- src/create-http-client.ts
+++ src/create-http-client.ts
@@ -22,13 +22,14 @@ declare let window: any;
 export default function createHttpClient(options: MollieRequestConfig) {
   options.baseURL = 'https://api.mollie.com:443/v2/';
 
-  options.headers = Object.assign({}, options.headers, {
+  options.headers = {
+    ...options.headers,
     Authorization: `Bearer ${options.apiKey}`,
     'Accept-Encoding': 'gzip',
     'Content-Type': 'application/json',
     'User-Agent': `node.js/${process.version}`,
     'X-Mollie-User-Agent': `mollie/${version}`,
-  });
+  };
 
   // Setting the root CA certificate will fail in a browser environment
   if (typeof window !== 'undefined') {
