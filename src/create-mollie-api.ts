853aa17cedf90e8674c988f6e8172e711ee6d42b
Michael Dekker <git@michaeldekker.nl>
2019-01-09 03:26:53 +0100

    Activate capture endpoint

--- src/create-mollie-api.ts
+++ src/create-mollie-api.ts
@@ -14,6 +14,7 @@ import OrdersShipmentsResource from './resources/orders/shipments';
 import OrdersRefundsResource from './resources/orders/refunds';
 import OrdersResource from './resources/orders/orders';
 import OrdersLinesResource from './resources/orders/lines';
+import PaymentsCapturesResource from './resources/payments/captures';
 
 interface ICreateMollieApi {
   httpClient: AxiosInstance;
@@ -26,7 +27,7 @@ export interface IMollieApiClient {
   refunds: RefundsResource;
   payments_chargebacks: PaymentsChargebacksResource;
   chargebacks: ChargebacksResource;
-  payments_captures: null; // TODO: implement
+  payments_captures: PaymentsCapturesResource;
   customers: CustomersResource;
   customers_payments: CustomersPaymentsResource;
   customers_mandates: CustomersMandatesResource;
@@ -57,7 +58,7 @@ export default function createMollieApi({ httpClient }: ICreateMollieApi): IMoll
     payments_chargebacks: new PaymentsChargebacksResource(httpClient),
     chargebacks: new ChargebacksResource(httpClient),
     // Captures API
-    payments_captures: null, // TODO: implement the Captures API
+    payments_captures: new PaymentsCapturesResource(httpClient),
 
     // Customers API
     customers: new CustomersResource(httpClient),
