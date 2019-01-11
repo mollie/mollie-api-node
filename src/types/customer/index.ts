e5da956d70c14af3c6a54d98ba027c2987c16a45
Michael Dekker <git@michaeldekker.nl>
2019-01-11 02:11:00 +0100

    Update customer type

--- src/types/customer/index.ts
+++ src/types/customer/index.ts
@@ -1,9 +1,9 @@
-import { ApiMode, ILinks, Locale, PaymentMethod } from '../global';
+import { ApiMode, ILinks, Locale, PaymentMethod, IUrl } from '../global';
 
 /**
  * Customer Response object.
  *
- * @see
+ * @see http://localhost:63342/api-documentation/build/html/reference/v2/customers-api/get-customer.html
  */
 export interface ICustomer {
   resource: string;
@@ -15,8 +15,21 @@ export interface ICustomer {
   recentlyUsedMethods: Array<PaymentMethod>;
   metadata: any;
   createdAt: string;
-  _links: ILinks;
+  _links: ICustomerLinks;
 
   // Access token parameters
   testmode?: boolean;
+}
+
+/**
+ * Customer _links object
+ *
+ * @param mandates - Mandates list
+ * @param subscriptions - Subscriptions list
+ * @param payments - Payments list
+ */
+export interface ICustomerLinks extends ILinks {
+  mandates: IUrl;
+  subscriptions: IUrl;
+  payments: IUrl;
 }
