0706b2bff2d185dbe4be8b4cfcff60aaf0e1b817
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-08-07 12:13:36 +0200

    Rollup now runs the code through Babel.
    If we are going to support older versions of Node.js, we have to either:
     1) make sure we're only using language features supported by those versions, or
     2) run the code through Babel.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,31 @@
 import json from 'rollup-plugin-json';
-import typescript from 'rollup-plugin-typescript2';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy-glob';
 
 export default {
   input: 'src/mollie.ts',
-  external: ['fs', 'https', 'path', 'axios', 'qs', 'lodash/omit', 'lodash/toPlainObject'],
-  output: [{ file: 'dist/mollie.cjs.js', format: 'cjs' }, { file: 'dist/mollie.esm.js', format: 'es' }],
+  external: [
+    // These Node.js interenals are external to our bundles…
+    ...['fs', 'https', 'path', 'url'],
+    // …as are the dependencies listed in our package.json.
+    ...Object.keys(require('./package.json').dependencies)
+  ],
+  output: [
+    { file: 'dist/mollie.cjs.js', format: 'cjs' },
+    { file: 'dist/mollie.esm.js', format: 'es' }
+  ],
   plugins: [
     json(),
-    typescript({
-      typescript: require('typescript'),
+    resolve({
+      extensions: ['.ts'],
+      customResolveOptions: {
+        moduleDirectory: 'src',
+      },
+      preferBuiltins: true
+    }),
+    babel({
+      extensions: ['.ts']
     }),
     copy([{ files: 'src/cacert.pem', dest: 'dist' }]),
   ],
