aaac3fcf2fdc4c322f83146d5b7e820ca1c91f58
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2019-08-09 11:50:11 +0200

    In the Capture object, define the shipment and settlement links as well.
    This improved the exported type definition slightly.

--- src/models/Capture.ts
+++ src/models/Capture.ts
@@ -19,6 +19,8 @@ export default class Capture extends Model implements ICapture {
   public _links = {
     self: null,
     payment: null,
+    shipment: null,
+    settlement: null,
     documentation: null,
   };
   public shipmentId = null;
