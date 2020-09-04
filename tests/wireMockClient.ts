ae3705fc4ecd5b56eafc9e7735ad43331a25f4f6
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2020-09-04 18:12:08 +0200

    Add apiEndpoint option.

--- tests/wireMockClient.ts
+++ tests/wireMockClient.ts
@@ -1,16 +1,16 @@
-import createMollieClient, { MollieClient } from '..';
+import createMollieClient, { MollieClient, MollieOptions } from '..';
 import { AxiosInstance } from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import {} from 'jest-bluster';
 
-export default function wireMockClient(versionStrings?: string | string[]): { adapter: MockAdapter; client: MollieClient } {
+export default function wireMockClient(options?: Omit<MollieOptions, 'accessToken' | 'adapter' | 'apiKey'>): { adapter: MockAdapter; client: MollieClient } {
   const adapter = new MockAdapter((undefined as unknown) as AxiosInstance);
   return {
     adapter,
     client: createMollieClient({
       apiKey: 'mock-api-key',
       adapter: adapter.adapter(),
-      versionStrings,
+      ...options,
     }),
   };
 }
