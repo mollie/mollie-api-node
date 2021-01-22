569eefcc2bc26155702e3a9b50661beb1caceec9
Hob Adams <hello@hobadams.com>
2021-01-22 14:23:08 +0000

    remove unused file

--- src/resources/chargebacks/parameters.ts
+++
@@ -1,6 +0,0 @@
-import { ChargebackEmbed } from '../../data/chargebacks/Chargeback';
-import { CommonListParameters } from '../../types/parameters';
-
-export type ListParameters = CommonListParameters & {
-  embed?: ChargebackEmbed[];
-};
