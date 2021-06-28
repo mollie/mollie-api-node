216445fbd560f004abd648ae91a7d739319d2aa6
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2021-06-28 12:07:48 +0200

    Fix atsee URIs.

--- src/resources/customers/mandates/parameters.ts
+++ src/resources/customers/mandates/parameters.ts
@@ -27,18 +27,16 @@ export type CreateParameters = ContextParameters &
      */
     consumerBic?: string;
     /**
+     * The consumer’s email address. Required for paypal mandates
+     *
+     * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=consumerEmail#parameters
+     */
+    consumerEmail?: string;
+    /**
      * The date when the mandate was signed in `YYYY-MM-DD` format.
      *
      * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=signatureDate#parameters
      */
-
-    /**
-     * The consumer’s email address. Required for paypal mandates
-     *
-     * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=signatureDate#parameters
-     */
-    consumerEmail?: string;
-
     signatureDate?: string;
     /**
      * A custom mandate reference. Use an unique `mandateReference` as some banks decline a Direct Debit payment if the `mandateReference` is not unique.
@@ -46,12 +44,11 @@ export type CreateParameters = ContextParameters &
      * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=mandateReference#parameters
      */
     mandateReference?: string;
-
     /**
      * The billing agreement ID given by PayPal. For example: "B-12A34567B8901234CD".
      * Required for paypal mandates
      *
-     * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=signatureDate#parameters
+     * @see https://docs.mollie.com/reference/v2/mandates-api/create-mandate?path=paypalBillingAgreementId#parameters
      */
     paypalBillingAgreementId?: string;
   };
