2e788ac547f25250cd014453303e468573265947
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2021-11-15 19:18:02 +0100

    Exclude Nockful tests in Node.js < 8.

--- jest.config.js
+++ jest.config.js
@@ -9,15 +9,21 @@ function createProject(displayName, testRegex, rest) {
   });
 }
 
+const projects = [
+  createProject('nockless tests', `/tests/(?!${nockfulTests.join('|')}).*/.+\\.test\\.[jt]s$`)
+];
+
+// Only execute the Nockful tests on Node.js 8+.
+if (parseInt(process.version.substring(1)) >= 8) {
+  projects.push(createProject('nockful tests', `/tests/(?:${nockfulTests.join('|')}).*/.+\\.test\\.[jt]s$`, {
+    // Use this custom runner. It's a normal runner, but by specifying a custom runner, it won't be shared with the
+    // other project.
+    runner: './tests/runner.js',
+  }));
+}
+
 module.exports = {
-  projects: [
-    createProject('nockless tests', `/tests/(?!${nockfulTests.join('|')}).*/.+\\.test\\.[jt]s$`),
-    createProject('nockful tests', `/tests/(?:${nockfulTests.join('|')}).*/.+\\.test\\.[jt]s$`, {
-      // Use this custom runner. It's a normal runner, but by specifying a custom runner, it won't be shared with the
-      // other project.
-      runner: './tests/runner.js',
-    })
-  ],
+  projects,
   transform: {
     '\\.[jt]s$': 'babel-jest'
   }
