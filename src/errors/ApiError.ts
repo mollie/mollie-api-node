mollie/mollie-api-node

commit eb0abd2c1e7c230e8f6e6634ce68a134e7bce6c5

Deprecate predictable helper methods.
This includes payment.isOpen (which is synonymous with payment.status == PaymentStatus.open); list.count (which is synonymous with list.length); and error.getMessage (which is synonymous with error.message).

src/errors/ApiError.ts
@@ -5,7 +5,7 @@ import { MollieApiErrorLinks, Url } from '../data/global';
 import Maybe from '../types/Maybe';
 
 export default class ApiError extends Error {
-  public constructor(message: string, protected title?: string, protected status?: number, protected field?: string, protected links?: MollieApiErrorLinks) {
+  public constructor(message: string, protected title?: string, public readonly statusCode?: number, public readonly field?: string, protected links?: MollieApiErrorLinks) {
     super(message);
     // Set the name to ApiError.
     this.name = 'ApiError';
@@ -17,6 +17,7 @@ export default class ApiError extends Error {
    * Get the error message
    *
    * @since 3.0.0
+   * @deprecated Use `error.message` instead.
    */
   public getMessage(): string {
     return this.message;
@@ -26,6 +27,7 @@ export default class ApiError extends Error {
    * Get the field name that contains an error
    *
    * @since 3.0.0
+   * @deprecated Use `error.field` instead.
    */
   public getField(): Maybe<string> {
     return this.field;
@@ -35,9 +37,10 @@ export default class ApiError extends Error {
    * Get the API status code
    *
    * @since 3.0.0
+   * @deprecated Use `error.statusCode` instead.
    */
   public getStatusCode(): Maybe<number> {
-    return this.status;
+    return this.statusCode;
   }
 
   /**
@@ -96,7 +99,7 @@ export default class ApiError extends Error {
     return new ApiError(
       get(response, 'data.detail', 'Received an error without a message'),
       get(response, 'data.title'),
-      get(response, 'data.status'),
+      get(response, 'data.statusCode'),
       get(response, 'data.field'),
       cloneDeep(get(response, 'data._links')),
     );

src/createMollieClient.ts
@@ -158,3 +158,5 @@ export { OrderLineType } from './data/orders/orderlines/OrderLine';
 export { PaymentEmbed, PaymentStatus } from './data/payments/data';
 export { RefundEmbed, RefundStatus } from './data/refunds/data';
 export { SubscriptionStatus } from './data/subscription/data';
+export { ProfileStatus } from './data/profiles/data';
+export { OnboardingStatus } from './data/onboarding/data';

src/data/customers/mandates/MandateHelper.ts
@@ -13,6 +13,8 @@ export default class MandateHelper extends Helper<MandateData, Mandate> {
 
   /**
    * Returns whether the mandate is valid.
+   *
+   * @deprecated Use `mandate.status == MandateStatus.valid` instead.
    */
   public isValid(this: MandateData): boolean {
     return this.status === MandateStatus.valid;

src/data/list/List.ts
@@ -4,6 +4,9 @@ import { Links, Url } from '../global';
 
 export default interface List<T> extends Array<T> {
   links: ListLinks;
+  /**
+   * @deprecated Use `list.length` instead.
+   */
   count: number;
   nextPageCursor: Maybe<string>;
   previousPageCursor: Maybe<string>;

src/data/onboarding/OnboardingHelper.ts
@@ -3,7 +3,7 @@ import TransformingNetworkClient from '../../TransformingNetworkClient';
 import Callback from '../../types/Callback';
 import Helper from '../Helper';
 import Organization, { OrganizationData } from '../organizations/Organizations';
-import { OnboardingData } from './data';
+import { OnboardingData, OnboardingStatus } from './data';
 import Onboarding from './Onboarding';
 
 export default class OnboardingHelper extends Helper<OnboardingData, Onboarding> {
@@ -11,16 +11,25 @@ export default class OnboardingHelper extends Helper<OnboardingData, Onboarding>
     super(networkClient, links);
   }
 
+  /**
+   * @deprecated Use `onboarding.status == OnboardingStatus.needsData` instead.
+   */
   public needsData(this: OnboardingData) {
-    return this.status == 'needs-data';
+    return this.status == OnboardingStatus.needsData;
   }
 
+  /**
+   * @deprecated Use `onboarding.status == OnboardingStatus.inReview` instead.
+   */
   public isInReview(this: OnboardingData) {
-    return this.status == 'in-review';
+    return this.status == OnboardingStatus.inReview;
   }
 
+  /**
+   * @deprecated Use `onboarding.status == OnboardingStatus.completed` instead.
+   */
   public isCompleted(this: OnboardingData) {
-    return this.status == 'completed';
+    return this.status == OnboardingStatus.completed;
   }
 
   /**

src/data/onboarding/data.ts
@@ -28,7 +28,7 @@ export interface OnboardingData extends Model<'onboarding', undefined> {
    *
    * @see https://docs.mollie.com/reference/v2/onboarding-api/get-onboarding-status?path=status#response
    */
-  status: 'needs-data' | 'in-review' | 'completed';
+  status: OnboardingStatus;
   /**
    * Whether or not the organization can receive payments.
    *
@@ -62,4 +62,10 @@ export interface OnboardingLinks extends Links {
    * @see https://docs.mollie.com/reference/v2/onboarding-api/get-onboarding-status?path=_links/organization#response
    */
   organization: Url;
+}
+
+export enum OnboardingStatus {
+  needsData = 'needs-data',
+  inReview = 'in-review',
+  completed = 'completed',
 }

src/data/payments/PaymentHelper.ts
@@ -25,6 +25,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
 
   /**
    * Returns whether the payment has been created, but nothing else has happened with it yet.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.open` instead.
    */
   public isOpen(this: PaymentData): boolean {
     return this.status === PaymentStatus.open;
@@ -32,6 +34,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
 
   /**
    * Returns whether new captures can be created for this payment.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.authorized` instead.
    */
   public isAuthorized(this: PaymentData): boolean {
     return this.status === PaymentStatus.authorized;
@@ -46,6 +50,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
 
   /**
    * Returns whether the payment has been canceled by the customer.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.canceled` instead.
    */
   public isCanceled(this: PaymentData): boolean {
     return this.status == PaymentStatus.canceled;
@@ -53,6 +59,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
 
   /**
    * Returns whether the payment has expired, e.g. the customer has abandoned the payment.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.expired` instead.
    */
   public isExpired(this: PaymentData): boolean {
     return this.status == PaymentStatus.expired;
@@ -76,6 +84,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
 
   /**
    * Returns whether the payment has failed and cannot be completed with a different payment method.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.failed` instead.
    */
   public isFailed(this: PaymentData): boolean {
     return this.status == PaymentStatus.failed;
@@ -84,6 +94,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
   /**
    * Returns whether the payment is in this temporary status that can occur when the actual payment process has been
    * started, but has not completed yet.
+   *
+   * @deprecated Use `payment.status == PaymentStatus.pending` instead.
    */
   public isPending(this: PaymentData): boolean {
     return this.status == PaymentStatus.pending;
@@ -106,6 +118,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
   /**
    * Returns whether `sequenceType` is set to `'first'`. If a `'first'` payment has been completed successfully, the
    * consumer's account may be charged automatically using recurring payments.
+   *
+   * @deprecated Use `payment.sequenceType == SequenceType.first` instead.
    */
   public hasSequenceTypeFirst(this: PaymentData): boolean {
     return this.sequenceType == SequenceType.first;
@@ -114,6 +128,8 @@ export default class PaymentHelper extends Helper<PaymentData, Payment> {
   /**
    * Returns whether `sequenceType` is set to `'recurring'`. This type of payment is processed without involving the
    * consumer.
+   *
+   * @deprecated Use `payment.sequenceType == SequenceType.recurring` instead.
    */
   public hasSequenceTypeRecurring(this: PaymentData): boolean {
     return this.sequenceType == SequenceType.recurring;

src/data/profiles/ProfileHelper.ts
@@ -9,7 +9,7 @@ import { PaymentData } from '../payments/data';
 import Payment from '../payments/Payment';
 import { RefundData } from '../refunds/data';
 import Refund from '../refunds/Refund';
-import { ProfileData } from './data';
+import { ProfileData, ProfileStatus } from './data';
 import Profile from './Profile';
 
 export default class ProfileHelper extends Helper<ProfileData, Profile> {
@@ -17,16 +17,25 @@ export default class ProfileHelper extends Helper<ProfileData, Profile> {
     super(networkClient, links);
   }
 
+  /**
+   * @deprecated Use `profile.status == ProfileStatus.unverified` instead.
+   */
   public isUnverified(this: ProfileData) {
-    return this.status == 'unverified';
+    return this.status == ProfileStatus.unverified;
   }
 
+  /**
+   * @deprecated Use `profile.status == ProfileStatus.verified` instead.
+   */
   public isVerified(this: ProfileData) {
-    return this.status == 'verified';
+    return this.status == ProfileStatus.verified;
   }
 
+  /**
+   * @deprecated Use `profile.status == ProfileStatus.blocked` instead.
+   */
   public isBlocked(this: ProfileData) {
-    return this.status == 'blocked';
+    return this.status == ProfileStatus.blocked;
   }
 
   /**

commit d3e5c6811c8ed524f1950181ddc2872e6ec05883

Fix bug related to errors I introduced in eb0abd2c.

src/errors/ApiError.ts
@@ -99,7 +99,7 @@ export default class ApiError extends Error {
     return new ApiError(
       get(response, 'data.detail', 'Received an error without a message'),
       get(response, 'data.title'),
-      get(response, 'data.statusCode'),
+      get(response, 'data.status'),
       get(response, 'data.field'),
       cloneDeep(get(response, 'data._links')),
     );