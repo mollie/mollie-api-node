fd4a38e52279970206e2298b79e26dae794464b1
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2021-11-16 17:07:28 +0100

    Add comment about mockNetwork.

--- tests/iteration/iteration.test.ts
+++ tests/iteration/iteration.test.ts
@@ -5,6 +5,8 @@ import { setupRecorder } from 'nock-record';
 import { run } from 'ruply';
 import getAccessToken from '../getAccessToken';
 
+// false ‒ This test interacts with the real Mollie API over the network, and records the communication.
+// true  ‒ This test uses existing recordings to simulate the network.
 const mockNetwork = true;
 
 describe('iteration', () => {
