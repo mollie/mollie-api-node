2ec0d1a29d053f6d7f1294d308b55d56289948ea
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2022-08-29 15:47:12 +0200

    Add profileId and testMode to ListParameters where appropriate.

--- src/binders/refunds/parameters.ts
+++ src/binders/refunds/parameters.ts
@@ -1,5 +1,8 @@
 import { PaginationParameters, ThrottlingParameters } from '../../types/parameters';
 
-export type ListParameters = PaginationParameters;
+export type ListParameters = PaginationParameters & {
+  profileId?: string;
+  testmode?: boolean;
+};
 
 export type IterateParameters = Omit<ListParameters, 'limit'> & ThrottlingParameters;
