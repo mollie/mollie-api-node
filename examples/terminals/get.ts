d092a8dff02db5c39b4a66e29a4f4bbcb3b0f292
Jan Paepke <jan@paepke.at>
2025-03-17 18:50:38 +0100

    fix examples

--- examples/terminals/get.ts
+++ examples/terminals/get.ts
@@ -1,13 +1,13 @@
 /**
  * @docs https://docs.mollie.com/reference/v2/terminals-api/get-terminal
  */
-import createMollieClient, { Terminal } from '@mollie/api-client';
+import createMollieClient from '@mollie/api-client';
 
 const mollieClient = createMollieClient({ apiKey: 'test_dHar4XY7LxsDOtmnkVtjNVWXLSlXsM' });
 
 (async () => {
   try {
-    const terminal: Terminal = await mollieClient.terminals.get('term_7MgL4wea46qkRcoTZjWEH');
+    const terminal = await mollieClient.terminals.get('term_7MgL4wea46qkRcoTZjWEH');
 
     console.log(terminal);
   } catch (error) {
