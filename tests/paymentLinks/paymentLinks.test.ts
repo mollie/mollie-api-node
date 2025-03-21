2fc4227d61a0681fe04833390c2d6810e31c2bb3
Jan Paepke <jan@paepke.at>
2025-03-21 19:18:22 +0100

    fix tests

--- tests/paymentLinks/paymentLinks.test.ts
+++ tests/paymentLinks/paymentLinks.test.ts
@@ -1,6 +1,6 @@
 import { MollieClient } from '../..';
-import NetworkMocker, { getAccessTokenClientProvider } from '../NetworkMocker';
 import getHead from '../getHead';
+import NetworkMocker, { getAccessTokenClientProvider } from '../NetworkMocker';
 
 // 'record' ‒ This test interacts with the real Mollie API over the network, and records the communication.
 // 'replay' ‒ This test uses existing recordings to simulate the network.
@@ -24,7 +24,7 @@ describe('paymentLinks', () => {
     });
     expect(paymentLink.getPaymentUrl()).toMatch(/^https:\/\/paymentlink\.mollie\.com\//);
     paymentLink = await mollieClient.paymentLinks.get(paymentLink.id);
-    expect(paymentLink.amount.value).toBe('6.99');
+    expect(paymentLink.amount?.value).toBe('6.99');
   });
 
   test('paymentLinks error response', async () => {
@@ -32,14 +32,14 @@ describe('paymentLinks', () => {
   });
 
   test('paymentLink payments', async () => {
-    const paymentLink = await mollieClient.paymentLinks.get('pl_aTMXG7OQ2CS6VhsktUiSB')
+    const paymentLink = await mollieClient.paymentLinks.get('pl_fake_with_payments');
 
     const payments = paymentLink.getPayments();
 
-    const payment = await getHead(payments.take(1))
+    const payment = await getHead(payments.take(1));
 
-    expect(payment.id).toBe('tr_5B8cwPMGnU6qLbRvo7qEZo')
-  })
+    expect(payment.id).toBe('tr_5B8cwPMGnU6qLbRvo7qEZo');
+  });
 
   afterAll(() => networkMocker.cleanup());
 });
