877762a5127ce4728571206180b1601dfcbc4635
Jan Paepke <jan@paepke.at>
2025-04-02 08:30:47 +0200

    skip tests for now

--- tests/integration/terminals.test.ts
+++ tests/integration/terminals.test.ts
@@ -9,7 +9,9 @@ dotenv.config();
 const mollieClient = createMollieClient({ apiKey: process.env.API_KEY });
 
 describe('terminals', () => {
-  it('should integrate', async () => {
+  // TODO: This test is skipped for now because it requires adding a terminal to the test account and thus potentially a dedicated API key.
+  // https://github.com/mollie/mollie-api-node/pull/413?notification_referrer_id=NT_kwDOAC7wUbMxNTM1MTQzMjMwMDozMDc2MTc3#issuecomment-2765899992
+  it.skip('should integrate', async () => {
     const terminals = await mollieClient.terminals.page();
 
     expect(terminals).toBeDefined();
